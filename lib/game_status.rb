79083e3505a0291cc1c18bf54b5df267ba668079
Kachel Wittig <kjwittig@gmail.com>
2018-03-02 20:21:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,8 +26,6 @@ def won?(board)
       position3 = board[win_combo[2]]
       if (position1 == "X" && position2 == "X" && position3 == "X") || (position1 == "O" && position2 == "O" && position3 == "O")
         return win_combo
-      else
-        false
       end
       # hint -->  .detect and .all
   end
